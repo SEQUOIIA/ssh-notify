6038b6f74c923a22a959457188f5a27cbd3291d9
Emil H. Clausen (SEQUOIIA) <sequoiia@hummel.yt>
2019-03-10 20:34:19 +0100

    Added reqwest, discord webhook added

--- src/agent/discord.rs
+++ src/agent/discord.rs
@@ -7,7 +7,16 @@ pub struct Discord {
 }
 
 impl Discord {
-    pub fn run(&self, vars : model::Vars) {
+    pub fn run(self, vars : model::Vars) {
+        let client = reqwest::ClientBuilder::new()
+            .build()
+            .unwrap();
 
+        let url = self.data.webhook_url.unwrap();
+
+        let mut res = client.post(url.as_str())
+            .header("Content-Type", "application/json")
+            .body(format!("{{\"content\": \"SSH Login: {} from {} on {}\"}}", vars.user, vars.r_host, ""))
+            .send().unwrap();
     }
 }
