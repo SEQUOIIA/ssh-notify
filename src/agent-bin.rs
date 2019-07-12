257d48816f6ae1e2e27e920750bf414b6a2ed739
Emil H. Clausen <sequoiia@hummel.yt>
2019-07-12 13:53:39 +0200

    Begun work on separation of main functionality and the functionality that uses the providers
    This is done in order to be able to run the provider code as a child process, hopefully avoiding locking up/slowing down the SSH login process due to external factors(slow HTTP requests, SMTP, etc.)

---
+++ src/agent-bin.rs
@@ -0,0 +1,44 @@
+extern crate pretty_env_logger;
+extern crate log;
+extern crate toml;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate reqwest;
+extern crate lettre;
+extern crate lettre_email;
+extern crate hostname;
+extern crate ipnet;
+#[macro_use]
+extern crate log4rs;
+
+mod config;
+mod model;
+pub mod agent;
+
+use std::env::{var};
+
+fn main() {
+    let conf = config::config();
+    let mut vars = get_pam_vars();
+
+    if let Some(agents) = conf.agents {
+        for ag in agents {
+            match ag {
+                _ => {
+                    ag.send(vars.clone()).unwrap();
+                }
+            }
+        }
+    }
+}
+
+fn get_pam_vars() -> model::Vars {
+    let user = var("PAM_USER").expect("PAM ENV(PAM_USER) not found");
+    let rhost = var("PAM_RHOST").expect("PAM ENV(PAM_RHOST) not found");
+    let pamtype = var("PAM_TYPE").expect("PAM ENV(PAM_TYPE) not found");
+    let hostname_v = hostname::get_hostname().unwrap();
+    let is_whitelisted : bool = false;
+
+    model::Vars {user, r_host: rhost, hostname: hostname_v, pam_type: pamtype, is_whitelisted}
+}
